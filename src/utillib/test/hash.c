4c79de6b2e3fe2e633a549730fb45853eab82d35
cgsdfc <cgsdfc@126.com>
2017-11-03 01:03:29 +0800

    ignored autotool files

---
+++ src/utillib/test/hash.c
@@ -0,0 +1,12 @@
+#include <utillib/hash.h>
+#include <utillib/test.h>
+
+UTILLIB_TEST(hash_string) {}
+
+UTILLIB_TEST_DEFINE(Utillib_Hash) {
+  UTILLIB_TEST_BEGIN(Utillib_Hash)
+    UTILLIB_TEST_RUN(hash_string)
+    UTILLIB_TEST_END(Utillib_Hash)
+    UTILLIB_TEST_RETURN(Utillib_Hash)
+    }
+
