a84bbd8c9d1d95fa090d566362ee712b2d9b080b
cgsdfc <cgsdfc@126.com>
2017-11-13 19:48:26 +0800

    add config.h

---
+++ src/utillib/config.h
@@ -0,0 +1,12 @@
+#ifndef UTILLIB_CONFIG_H
+#define UTILLIB_CONFIG_H
+
+# ifdef __MSC_VER 
+typedef int bool
+# define true (1)
+# define false (0)
+# else
+# include <stdbool.h>
+# endif
+
+#endif /* UTILLIB_CONFIG_H */
