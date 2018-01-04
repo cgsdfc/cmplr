fe9a3cec242af894c3ac138786cd8117a9f3e905
cgsdfc <cgsdfc@126.com>
2018-01-05 01:26:45 +0800

    Write member ctr dtr of AST

--- src/cling/AST.cpp
+++ src/cling/AST.cpp
@@ -10,3 +10,118 @@ ConstDecl::~ConstDecl() {
   delete type;
   delete constDefs;
 }
+
+ConstDef::ConstDef(TokenValue *name, TokenValue *value)
+  :name(static_cast<Identifier*>(name)),
+  value(static_cast<IntegralToken*>(value)){}
+
+ConstDef::~ConstDef() {
+  delete name;
+  delete value;
+}
+
+VarDef::VarDef(TokenValue *name, Type *type)
+  :name(static_cast<Identifier*> (name)),
+  type(type){}
+
+VarDef::~VarDef() {
+  delete name;
+  delete type;
+}
+
+VarDecl::~VarDecl() {
+  delete type;
+  delete varDefs;
+}
+
+FunctionDecl::~FunctionDecl() {
+  delete signature;
+  delete name;
+  delete statements;
+}
+
+FunctionType::~FunctionType() {
+  delete returnType;
+  delete argvTypes;
+}
+
+unsigned int Type::GetStorageSize() {
+  return 0;
+}
+
+unsigned int ArrayType::GetStorageSize() {
+  return extend * elementType->GetStorageSize();
+}
+
+bool FunctionType::SupportOperation(Operation const *operation) {
+  switch(operation->GetKind()) {
+    case OperationKind::CALL:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool ConstType::SupportOperation(Operation const *operation) {
+  switch (operation->GetKind()) {
+    case  OperationKind::ASSIGN:
+    case OperationKind::SCANF:
+      return true;
+    default:
+      return baseType->SupportOperation(operation);
+  }
+}
+
+bool ArrayType::SupportOperation(Operation const *operation) {
+  switch(operation->GetKind()) {
+    case OperationKind::INDEX:
+      return true;
+    default:
+      return false;
+  }
+}
+
+BinaryExpression::~BinaryExpression() {
+  delete operation;
+  delete leftOperand;
+  delete rightOperand;
+}
+
+UnaryExpression::~UnaryExpression() {
+  delete operation;
+  delete operand;
+}
+
+CallExpression::~CallExpression() {
+  delete callee;
+  delete args;
+}
+
+IfStatement::~IfStatement() {
+  delete condition;
+  delete thenClause;
+  delete elseClause;
+}
+
+ForStatement::~ForStatement() {
+  delete initialExpression;
+  delete conditionExpression;
+  delete stepExpression;
+  delete clauseStatement;
+}
+
+CaseStatement::CaseStatement(TokenValue *c, Statement *s):
+  constant(static_cast<IntegralToken*>(c)),
+  statement(s){}
+
+CaseStatement::~CaseStatement() {
+  delete constant;
+  delete statement;
+}
+
+SwitchStatement::~SwitchStatement() {
+  delete selectiveExpression;
+  delete caseClauses;
+  delete defaultClause;
+}
+
--- src/cling/AST.h
+++ src/cling/AST.h
@@ -1,12 +1,11 @@
 #ifndef CLING_AST_HXX
 #define CLING_AST_HXX
 #include "Utility.h"
+#include "Token.h"
 struct ConstDecl;
 struct VarDecl;
 struct FunctionDecl;
 struct Type;
-struct MutableType;
-struct ImmutableType;
 struct FunctionType;
 struct ArrayType;
 struct ConstType;
@@ -14,8 +13,6 @@ struct VarType;
 struct ConstDef;
 struct VarDef;
 struct Statement;
-
-struct TokenValue;
 struct Program {
   GenericVector<ConstDecl> *constDecls;
   GenericVector<VarDecl> *varDecls;
@@ -38,60 +35,146 @@ struct ConstDecl {
 
 struct VarDecl {
   Type const *type;
-  GenericVector<VarDef> varDefs;
+  GenericVector<VarDef> *varDefs;
+  template<class Tp, class V>
+    VarDecl(Tp type, V varDefs):type(type), varDefs(varDefs){}
+  ~VarDecl();
 };
 
 struct VarDef {
-  TokenValue *iden;
-  MutableType *type;
+  Identifier *name;
+  Type *type;
+  VarDef(TokenValue *name, Type *type);
+  ~VarDef();
 };
 
 struct ConstDef {
-  Type *type;
-  TokenValue *iden;
-  TokenValue *value;
-  ConstDef(TokenValue *iden, TokenValue *value):iden(iden), value(value){}
-  
+  Identifier *name;
+  IntegralToken *value;
+  ConstDef(TokenValue *name, TokenValue *value);
+  ~ConstDef();
 };
 
 struct FunctionDecl {
-  FunctionType *type;
-  TokenValue *iden;
+  FunctionType *signature;
+  TokenValue *name;
   GenericVector<Statement> *statements;
+  template<class Sig, class Nn, class Sta>
+    FunctionDecl(Sig signature, Nn name, Sta statements):
+      signature(signature), name(name), statements(statements) {}
+  ~FunctionDecl();
+};
+
+enum class OperationKind {
+  ARITH, 
+  CALL,
+  INDEX,
+  ASSIGN, 
+  PRINTF,
+  SCANF,
+};
+
+struct Operation {
+  unsigned int code;
+  Operation(unsigned int code):code(code){}
+  virtual ~Operation(){}
+  virtual OperationKind GetKind() const =0;
+};
+
+struct ArithmeticOperation: public Operation {
+  ArithmeticOperation(unsigned int code):Operation(code){}
+  ~ArithmeticOperation()=default;
+  OperationKind GetKind() const  override {
+    return OperationKind::ARITH;
+  }
+};
+
+struct AssignOperation: public Operation {
+  OperationKind GetKind() const  override {
+    return OperationKind::ASSIGN;
+  }
+};
+
+struct CallOperation: public Operation {
+  OperationKind GetKind() const  override {
+    return OperationKind::CALL;
+  }
 };
 
 struct Type {
+  virtual char *GetTypeName()=0;
+  virtual unsigned int GetStorageSize()=0;
+  virtual ~Type(){}
+
+  /*
+   * This has a default impl which always return 0
+   */
+  virtual bool SupportOperation(Operation const *operation)=0;
 
 };
 
-struct MutableType: public Type {
 
+struct ConstType: public Type {
+  Type *baseType;
+
+  ConstType(Type *baseType):baseType(baseType){}
+  ~ConstType() {
+    delete baseType;
+  }
+  bool SupportOperation(Operation const *operation)override;
+  unsigned int GetStorageSize() {
+    return Type::GetStorageSize();
+  }
 };
 
-struct ImmutableType: public Type {
-
-
+struct ArrayType: public Type {
+  Type *elementType;
+  unsigned int extend;
+  ArrayType(Type *elementType, unsigned int extend)
+    :elementType(elementType), extend(extend){}
+  ~ArrayType() {
+    delete elementType;
+  }
+  unsigned int GetStorageSize()override;
+  bool SupportOperation(Operation const *operation)override;
 };
 
-struct ConstType: public ImmutableType {
-
+struct FunctionType: public Type {
+  Type *returnType;
+  GenericVector<Type> *argvTypes;
+  template<class R, class A>
+    FunctionType(R returnType, A argvTypes):
+      returnType(returnType), argvTypes(argvTypes){}
+  ~FunctionType();
+  bool SupportOperation(Operation const *operation)override;
+  unsigned int GetStorageSize() {
+    return Type::GetStorageSize();
+  }
 };
 
-struct ArrayType: public MutableType {
+struct VarType: public Type {
+  Type *baseType;
 
-};
+  VarType(Type *baseType):baseType(baseType){}
+  ~VarType() {
+    delete baseType;
+  }
+  unsigned int GetStorageSize()override;
+  bool SupportOperation(Operation const* operation) override;
 
-struct FunctionType: public ImmutableType {
-
-};
-
-struct VarType: public MutableType {
 
 };
 
 struct ExpressionStatement;
 struct Statement {
+  virtual ~Statement(){}
 
+};
+
+struct CompStatement: public Statement {
+  GenericVector<Statement> statements;
+  CompStatement()=default;
+  ~CompStatement()=default;
 };
 
 struct IterativeStatement: public Statement {};
@@ -104,40 +187,105 @@ struct ReturnStatement: public JumpStatement {
   ExpressionStatement *returnExpression;
 };
 
+struct CaseStatement: public Statement {
+  IntegralToken *constant;
+  Statement *statement;
+
+  CaseStatement(TokenValue *c, Statement *s);
+  ~CaseStatement();
+};
+
 struct SwitchStatement: public SelectiveStatement {
   ExpressionStatement *selectiveExpression;
+  GenericVector<CaseStatement> *caseClauses;
+  Statement *defaultClause;
+
+  template<class Expr, class Cases, class Default>
+  SwitchStatement(Expr expr, Cases cases, Default def):
+    selectiveExpression(expr),
+    caseClauses(cases),
+    defaultClause(def){}
+  ~SwitchStatement();
 
 };
 
-struct IfStatement: public SelectiveStatement {};
+struct IfStatement: public SelectiveStatement {
+  ExpressionStatement *condition;
+  Statement *thenClause;
+  Statement *elseClause;
+  template<class C, class T, class E>
+  IfStatement(C condition, T thenClause, E elseClause):
+    condition(condition),
+    thenClause(thenClause),
+    elseClause(elseClause){}
+  ~IfStatement();
+};
 
 struct ForStatement: public IterativeStatement {
   ExpressionStatement *initialExpression;
   ExpressionStatement *conditionExpression;
   ExpressionStatement *stepExpression;
   Statement *clauseStatement;
+
+  template<class Init, class Cond, class Step, class Clause>
+    ForStatement(Init init, Cond cond, Step step, Clause clause):
+      initialExpression(init),
+      conditionExpression(cond),
+      stepExpression(step),
+      clauseStatement(clause) {}
+  ~ForStatement();
 };
 
 
-struct ExpressionStatement: public Statement {};
+struct ExpressionStatement: public Statement {
+  Type *valueType;
+  ExpressionStatement():valueType(nullptr) {}
+  virtual ~ExpressionStatement() {
+    delete valueType;
+  }
+};
 
 struct BinaryExpression: public ExpressionStatement {
-  int operation;
+  Operation *operation;
   ExpressionStatement *leftOperand;
   ExpressionStatement *rightOperand;
+
+  template<class Op, class L, class R>
+    BinaryExpression(Op op, L lhs, R rhs)
+    :operation(op), leftOperand(lhs), rightOperand(rhs){}
+  ~BinaryExpression();
+
 };
 
 struct UnaryExpression: public ExpressionStatement {
-  int operation;
+  Operation *operation;
   ExpressionStatement *operand;
+
+  template<class Op, class Operand>
+    UnaryExpression(Op op, Operand operand):operation(op), operand(operand){}
+  ~UnaryExpression();
 };
 
 struct CallExpression: public ExpressionStatement {
+  Identifier *callee;
+  GenericVector<ExpressionStatement> *args;
+
+  template<class C, class A>
+    CallExpression(C callee, A args):callee(callee), args(args){}
+  ~CallExpression();
+
+
   
 };
 
-struct AtomicExpression: public ExpressionStatement {};
+struct AtomicExpression: public ExpressionStatement {
+  TokenValue *token;
+  AtomicExpression(TokenValue *token):token(token){}
+  ~AtomicExpression() {
+    delete token;
+  }
+};
 
-struct ExpressionInParethesis: public ExpressionStatement {};
+struct ExpressionInParethesis: public ExpressionStatement { };
 
 #endif
